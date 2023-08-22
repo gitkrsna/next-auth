121a34a8c7204d83ada1175cb99f5e44a3b543cb
krsna <yogikkyadav@gmail.com>
2023-08-22 21:29:03 +0530

    bugfix

--- src/components/student/AddStudent.tsx
+++ src/components/student/AddStudent.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from 'react-hook-form'
-import { Button } from '../ui/Button'
+import { Button } from '../ui/button'
 
 const formSchema = z.object({
     username: z.string().min(2, {
