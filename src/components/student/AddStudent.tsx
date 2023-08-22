7f8bb10ea61c5f27be5d57bc0689ff0391cc3e92
krsna <yogikkyadav@gmail.com>
2023-08-22 21:03:39 +0530

    bugfix

--- src/components/student/AddStudent.tsx
+++ src/components/student/AddStudent.tsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 
-import { Button } from "@/components/ui/button"
 import {
     Form,
     FormControl,
@@ -15,6 +14,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from 'react-hook-form'
+import { Button } from '../ui/Button'
 
 const formSchema = z.object({
     username: z.string().min(2, {
