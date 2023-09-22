1789724900e1179a8a361c8757aad8b8f1c2ce65
krsna <yogikkyadav@gmail.com>
2023-09-22 14:01:47 +0530

    updated table type

--- types/tableTypes.ts
+++ types/tableTypes.ts
@@ -1,8 +1,8 @@
 import { Database } from 'types/supabase';
 
-export type User = Database['public']['Tables']['users']['Row'];
+export type User = Database['public']['Tables']['user']['Row'];
 export type Course = Database['public']['Tables']['courses']['Row'];
 export type Subject = Database['public']['Tables']['subjects']['Row'];
-export type Teacher = Database['public']['Tables']['teachers']['Row'] & {
-  users: User;
+export type Teacher = Database['public']['Tables']['teacher']['Row'] & {
+  user: User;
 };
