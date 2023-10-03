6ffc494908674c42357be5eee528eaacf573b0b0
krsna <yogikkyadav@gmail.com>
2023-10-03 21:52:10 +0530

    added gendatepicker

---
+++ src/components/shared-ui/GenDatePicker.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { format } from "date-fns"
+import { ControllerRenderProps, FieldValues } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    FormControl
+} from "@/components/ui/form"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+
+import { CalendarIcon } from '@radix-ui/react-icons'
+
+const GenDatePicker = ({ field }: { field: ControllerRenderProps<FieldValues, string> }) => {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <FormControl>
+                    <Button
+                        variant={"outline"}
+                        className={cn(
+                            "w-[100%] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                        )}
+                    >
+                        {field.value ? (
+                            format(field.value, "PPP")
+                        ) : (
+                            <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+export default GenDatePicker
