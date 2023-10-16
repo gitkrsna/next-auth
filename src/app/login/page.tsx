88ae484b95e8109afbfcc0a56da4a313653d340a
krsna <yogikkyadav@gmail.com>
2023-10-17 01:46:05 +0530

    styling added in login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,10 @@ import LoginForm from './LoginForm'
 
 const Login = () => {
     return (
-        <div className='flex justify-center items-center bg-black h-screen'>
-            <LoginForm />
+        <div className='flex justify-center items-center bg-black h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500' >
+            <div className='bg-black p-24 rounded-lg'>
+                <LoginForm />
+            </div>
         </div>
     )
 }
