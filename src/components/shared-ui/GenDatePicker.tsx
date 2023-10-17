da71b509adbd899f79901aad0533848f03d63db3
krsna <yogikkyadav@gmail.com>
2023-10-18 02:25:21 +0530

    bugfix

--- src/components/shared-ui/GenDatePicker.tsx
+++ src/components/shared-ui/GenDatePicker.tsx
@@ -9,7 +9,7 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
-import { parse } from "date-fns"
+import { isDate, parse } from "date-fns"
 import { ControllerRenderProps, FieldValues } from "react-hook-form"
 
 import { CalendarIcon } from '@radix-ui/react-icons'
@@ -17,7 +17,8 @@ import { Input } from '../ui/input'
 import { useState } from 'react'
 
 const GenDatePicker = ({ field, disabled }: { field: ControllerRenderProps<FieldValues, string>, disabled: boolean, placeholder: string }) => {
-    const [inputDate, setInputDate] = useState<string | undefined>(new Date(field.value).toLocaleDateString())
+    const initialDate = field.value ? new Date(field.value).toLocaleDateString() : ""
+    const [inputDate, setInputDate] = useState<string | undefined>(initialDate)
     return (
         <Popover>
             <div className='flex relative'>
