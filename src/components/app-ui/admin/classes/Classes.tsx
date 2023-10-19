103bee611bc9b3de23367902debdd70b08b3272f
krsna <yogikkyadav@gmail.com>
2023-10-19 23:07:41 +0530

    handle class name while editing

--- src/components/app-ui/admin/classes/Classes.tsx
+++ src/components/app-ui/admin/classes/Classes.tsx
@@ -1,53 +1,65 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import GenTable from '@/components/shared-ui/GenTable'
-import { useToast } from '@/components/ui/use-toast'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from 'types/supabase'
-import { Class } from 'types/tableTypes'
-import AddClass from './AddClass'
-import useClassColumns from './useClassColumns'
+import * as React from 'react';
+import GenTable from '@/components/shared-ui/GenTable';
+import { useToast } from '@/components/ui/use-toast';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from 'types/supabase';
+import { Class } from 'types/tableTypes';
+import AddClass from './AddClass';
+import useClassColumns from './useClassColumns';
 
 export function Classes() {
-    const { toast } = useToast()
+  const { toast } = useToast();
 
-    const [classes, setClasses] = React.useState<Class[]>([]);
+  const [classes, setClasses] = React.useState<Class[]>([]);
 
-    const fetchClasses = async () => {
-        const supabase = createClientComponentClient<Database>()
+  const fetchClasses = async () => {
+    const supabase = createClientComponentClient<Database>();
 
-        const { data, error } = await supabase
-            .from('classes')
-            .select('id, start_time, end_time, day_of_week, room, created_at, user(id, first_name, last_name), courses(id, name)');
+    const { data, error } = await supabase
+      .from('classes')
+      .select(
+        'id, name, start_time, end_time, day_of_week, room, created_at, user(id, first_name, last_name), courses(id, name)'
+      );
 
-        !error && setClasses(data as Class[])
+    !error && setClasses(data as Class[]);
+  };
 
+  const deleteClass = async (record: Class) => {
+    const supabase = createClientComponentClient<Database>();
+    const { error } = await supabase
+      .from('classes')
+      .delete()
+      .eq('id', record.id);
+    if (!error) {
+      toast({
+        description: 'Class deleted successfully',
+      });
+      fetchClasses();
+    } else {
+      toast({
+        variant: 'destructive',
+        description: 'Something went wrong, please try again later',
+      });
     }
+  };
 
-    const deleteClass = async (record: Class) => {
-        const supabase = createClientComponentClient<Database>();
-        const { error } = await supabase.from('classes').delete().eq("id", record.id)
-        if (!error) {
-            toast({
-                description: "Class deleted successfully"
-            })
-            fetchClasses();
-        } else {
-            toast({
-                variant: "destructive",
-                description: "Something went wrong, please try again later"
-            })
-        }
-    }
+  const columns = useClassColumns({
+    refreshClasses: fetchClasses,
+    deleteClass,
+  });
 
-    const columns = useClassColumns({ refreshClasses: fetchClasses, deleteClass })
+  React.useEffect(() => {
+    fetchClasses();
+  }, []);
 
-    React.useEffect(() => {
-        fetchClasses();
-    }, [])
-
-    return (
-        <GenTable columns={columns} data={classes} searchId="Class name" rightComponent={<AddClass refreshClasses={fetchClasses} />} />
-    )
+  return (
+    <GenTable
+      columns={columns}
+      data={classes}
+      searchId='Class name'
+      rightComponent={<AddClass refreshClasses={fetchClasses} />}
+    />
+  );
 }
