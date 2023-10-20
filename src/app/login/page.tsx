75d1d776e8712a56ba9d450d7ab4dd2835a22434
krsna <yogikkyadav@gmail.com>
2023-10-20 14:07:34 +0530

    style change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500">
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-violet-500 to-orange-300">
       <div className="bg-slate-50 p-24 rounded-lg">
         <LoginForm />
       </div>
