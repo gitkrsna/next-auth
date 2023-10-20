4cc1a884e4e8b3754654c78952057ca7f516ffee
krsna <yogikkyadav@gmail.com>
2023-10-20 14:18:59 +0530

    changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import LoginForm from "./LoginForm";
+import React from 'react';
+import LoginForm from './LoginForm';
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-violet-500 to-orange-300">
-      <div className="bg-slate-50 p-24 rounded-lg">
+    <div className='flex justify-center items-center h-screen bg-gradient-to-r from-violet-500 to-orange-300'>
+      <div className='bg-slate-50 p-24 rounded-lg'>
         <LoginForm />
       </div>
     </div>
